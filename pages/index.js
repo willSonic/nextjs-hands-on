cdcbf51b293db2c50ebea5098d91a33697bf24b1
Will Streeter <will.streeter@gmail.com>
2019-12-07 17:32:21 -0800

    Move up to chapter 2

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Index = () => (
+    <div>
+       <h1>Hello Next.js</h1>
+      <p>Lorem ipsum dolor sit, ....</p>
+      <p>Lorem ipsum dolor sit, ....</p>
+      <p>Lorem ipsum dolor sit, ....</p>
+  </div>);
+
+
+export default Index;
