93097f9f6df225b1f57fcd70c51d8a15c2b05a70
Will <will@obo.pm>
2019-12-08 12:59:17 -0800

    updates

---
+++ chapter3/universal-blog-landing/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+export default ({ children, title = 'The Universal Blog' }) => (
+    <div>
+      <Head>
+         <title>{title}</title>
+         <meta charSet="utf-8" />
+         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+         <link rel="stylesheet" href="/static/css/normalize.css" />
+         <link rel="stylesheet" href="/static/css/skeleton.css" />
+         <link rel="stylesheet" href="/static/css/app.css" />
+      </Head>
+
+      <h1>{title}</h1>
+      <footer>I'm your footer</footer>
+   </div>
+);
